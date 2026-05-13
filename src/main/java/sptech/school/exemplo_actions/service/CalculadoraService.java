81356afff45cbb71643e064d04d4d470befda2a4
ViniEguchi <vinicius.eguchi@gmail.com>
2026-05-12 21:42:11 -0300

    tests: adding new tests

--- src/main/java/sptech/school/exemplo_actions/service/CalculadoraService.java
+++ src/main/java/sptech/school/exemplo_actions/service/CalculadoraService.java
@@ -12,7 +12,8 @@ public class CalculadoraService {
     public double dividir (double a, double b) {
         if (b == 0) {
             throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST
+                    HttpStatus.BAD_REQUEST,
+                    "Divisão por zero não permitida"
             );
         }
 
